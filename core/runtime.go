7e11b46292c144c0accf5fc466f1d156db01981c
苏文广 <suwenguang@cainiao.com>
2021-07-27 21:55:42 +0800

    runtime util

--- core/runtime.go
+++ core/runtime.go
@@ -5,15 +5,7 @@ import (
 	"runtime"
 )
 
-type Runtime interface {
-	// 获取运行时的目录路径
-	GetRuntimeDir() string
-}
-
-type RuntimeUtil struct {
-}
-
-func (r *RuntimeUtil) GetRuntimeDir() (string, error) {
+func GetRuntimeDir() (string, error) {
 
 	_, filename, _, ok := runtime.Caller(1)
 	var cwdPath string
--- core/runtime_test.go
+++ core/runtime_test.go
@@ -1,10 +1,11 @@
 package util
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestRuntimeUtil_GetRuntimeDir(t *testing.T) {
-	util := RuntimeUtil{}
-	dir, err := util.GetRuntimeDir()
+	dir, err := GetRuntimeDir()
 	if err != nil {
 		t.Error(err)
 	}
