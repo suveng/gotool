58bbb75912eb5c9358134bb378925f21ef7ce0b8
苏文广 <suwenguang@sto.cn>
2021-07-28 18:00:28 +0800

    add test doc

--- core/email_test.go
+++ core/email_test.go
@@ -3,6 +3,13 @@ package util
 import "testing"
 
 func TestSendEmail(t *testing.T) {
-	SendEmail("suveng@163.com", "标题", "内容", "suveng@qq.com", "", "smtp.qq.com", "587", ConvertHTML)
+	// qq 测试
+	SendEmail("suveng@163.com", "标题", "<h1>html</h1>", "suveng@qq.com", "", "smtp.qq.com", "587", ConvertHTML)
 	SendEmail("suveng@163.com", "标题", "内容", "suveng@qq.com", "", "smtp.qq.com", "587", ConvertText)
+
+	// 网易测试
+	SendEmail("suveng@qq.com", "申通快递面试邀请", "HR面试邀请 请于20210801到上海青浦区申通大楼面试", "suveng@163.com", "", "smtp.163.com", "25", ConvertText)
+
+	// 网易不能发 HTML 内容, 需要验证还是什么, 自行根据提示的url 去配置
+	SendEmail("suveng@qq.com", "申通快递面试邀请", "<h1>HR面试邀请 请于20210801到上海青浦区申通大楼面试</h1> 欢迎来面试!", "suveng@163.com", "", "smtp.163.com", "25", ConvertHTML)
 }
